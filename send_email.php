86bd4f995c71b5604d59268977eb1df1c1e10587
2000years <sr341.edm@gmail.com>
2024-07-31 19:27:46 +0700

    send_email.php

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "sr341.edm@gmail.com";
+    $subject = "New Contact Message from $name";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
