f350409bb5420b4e6a07a026b9eea9df3543ca6d
2000years <sr341.edm@gmail.com>
2024-08-01 18:03:23 +0700

    send_email.php

--- send_email.php
+++ send_email.php
@@ -1,38 +1,38 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // รับข้อมูลจากฟอร์ม
+    
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // ที่อยู่อีเมล์ผู้รับ
+    
     $to = 'sr341.edm@gmail.com';
 
-    // หัวข้ออีเมล์
-    $subject = "New Contact Form Submission from $name";
+    
+    $subject = "New Contact from $name";
 
-    // เนื้อหาอีเมล์
+    
     $email_body = "
     <html>
     <head>
         <title>Contact Form Submission</title>
     </head>
     <body>
-        <h2>Contact Form Submission</h2>
-        <p><strong>Name:</strong> $name</p>
-        <p><strong>Email:</strong> $email</p>
-        <p><strong>Message:</strong></p>
+        <h2>Contact SR341</h2>
+        <p><strong>Name</strong> $name</p>
+        <p><strong>Email</strong> $email</p>
+        <p><strong>Message</strong></p>
         <p>$message</p>
     </body>
     </html>
     ";
 
-    // ตั้งค่า Headers
+    
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= "From: $email" . "\r\n";
 
-    // ส่งอีเมล์
+    
     if (mail($to, $subject, $email_body, $headers)) {
         echo 'Sent successfully!';
     } else {
